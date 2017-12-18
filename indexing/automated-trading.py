fe911e7783942ec3b6616ba9533bcfcc7277f6e0
gregm711 <gregm@wearevest.com>
2017-12-18 09:59:25 -0500

    allocation and indexing done

--- indexing/automated-trading.py
+++ indexing/automated-trading.py
@@ -1,10 +1,14 @@
 from binance.client import Client
 
-
+from binance.enums import *
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import pandas as pd 
 
+from index import get_indexed_df
+
+import time
 dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
@@ -14,37 +18,66 @@ binance_api_secret = os.environ.get("BINANCE_API_SECRET")
 
 client = Client(binance_api_key, binance_api_secret)
 
-# # get market depth
-# depth = client.get_order_book(symbol='BNBBTC')
+def main():
+	balances = pd.DataFrame(client.get_account()['balances'])
+	df = get_indexed_df()
+	balances['symbol'] = balances['asset']
 
-# # place market buy order
-# order = client.create_order(
-#     symbol='BNBBTC',
-#     side=Client.SIDE_BUY,
-#     type=Client.ORDER_TYPE_MARKET,
-#     quantity=100)
+	merged_df = pd.merge(balances, df, on='symbol')
+	merged_df['free']= pd.to_numeric(merged_df['free'], errors='coerce')
+	merged_df['price_usd']= pd.to_numeric(merged_df['price_usd'], errors='coerce')
+	merged_df['percentage_market_cap']= pd.to_numeric(merged_df['percentage_market_cap'], errors='coerce')
+	merged_df['price_usd']= pd.to_numeric(merged_df['price_usd'], errors='coerce')
+	merged_df['free']= pd.to_numeric(merged_df['free'], errors='coerce')
 
-# # get all symbol prices
-# prices = client.get_all_tickers()
+	total_value = get_account_value(merged_df)
 
-# # start trade websocket
-# def process_message(msg):
-#     print("message type: {}".format(msg['e']))
-#     print(msg)
-#     # do something
 
-# from binance.websockets import BinanceSocketManager
-# bm = BinanceSocketManager(client)
-# bm.start_aggtrade_socket(symbol='BNBBTC')
-# bm.start()
+	projection = project_coins(merged_df, total_value)
+	reallocate_coins(projection)
 
-from binance.enums import *
-order = client.create_test_order(
-    symbol='BNBBTC',
-    side=SIDE_BUY,
-    type=ORDER_TYPE_LIMIT,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=100,
-    price='0.00001')
 
-print(order)
+
+
+def get_account_value(df):
+	account_value = 0.0
+	for i, row in df.iterrows():
+		account_value += row['price_usd'] * row['free']
+	return account_value
+	
+	
+
+def project_coins(df, account_value):
+	df['dollar_allocation'] = df['percentage_market_cap'] * account_value
+	df['coin_allocation'] = df['dollar_allocation'] / df['price_usd']
+	return df
+
+
+
+def reallocate_coins(df):
+	df['allocation_diff'] =  df['coin_allocation'] - df['free']
+	for i, row in df.iterrows():
+		print(row['symbol'], row['allocation_diff'])
+		if row['allocation_diff'] <= 0:
+			sell_coins(row['symbol'], row['allocation_diff'])
+		elif row['allocation_diff'] >= 0:
+			buy_coins(row['symbol'], row['allocation_diff'])
+		time.sleep(10)
+	
+def sell_coins(symbol, quantity):
+	order = client.create_order(symbol= symbol + 'BTC',side=Client.SIDE_SELL,type=Client.ORDER_TYPE_MARKET,quantity=quantity)
+	print("sold coins! ",order)
+
+def buy_coins(symbol, quantity):
+	order = client.create_order(symbol= symbol + 'BTC',side=Client.SIDE_BUY,type=Client.ORDER_TYPE_MARKET,quantity=quantity)
+	print("bought coins! ",order)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
--- indexing/index.py
+++ indexing/index.py
@@ -1,20 +1,30 @@
 import requests
 import pandas as pd 
-
+import os 
 api_url = "https://api.coinmarketcap.com/v1/"
 
-def main():
+
+
+
+def get_indexed_df():
 	tickers = requests.get(api_url + "ticker/")
 	df = pd.DataFrame(tickers.json())
-	index(df)
+	df = index(df)
+	return df
 
 
 
 def index(df):
-	print(df.head(20))
+	df = df.head(10)
+	df['market_cap_usd']= pd.to_numeric(df['market_cap_usd'], errors='coerce')
+	df['percent_change_24h']= pd.to_numeric(df['percent_change_24h'], errors='coerce')
+	df['percentage_market_cap'] = (df['market_cap_usd'] / df['market_cap_usd'].sum())
+	total_day_change =((df['percent_change_24h'] * df['percentage_market_cap']).sum())/(len(df.index))
+	return df
+
 
 
 
 
 if __name__ == "__main__":
-	main()
+	get_indexed_df()
