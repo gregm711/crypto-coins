d852e19896ab46484f680bd148aee98ae88fec15
gregm711 <gregm@wearevest.com>
2018-01-02 14:35:05 -0500

    coming along

---
+++ arbitraging/arbitrage.py
@@ -0,0 +1,66 @@
+from binance.client import Client
+import numpy
+from binance.enums import *
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import pandas as pd 
+
+import math
+import time
+
+import poloniex
+
+
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
+
+binance_api_key = os.environ.get("BINANCE_API_KEY")
+binance_api_secret = os.environ.get("BINANCE_API_SECRET")
+poloniex_api_key = os.environ.get("POLONIEX_API_KEY")
+poloniex_api_secret = os.environ.get("POLONIEX_API_SECRET")
+
+poloniex_bitcoin_withdrawl_fee = 0.0005
+binance_bitcoin_withdrawl_fee = 0.001
+
+binance_trading_fee = 0.001
+poloniex_trading_fee = 0.0015
+
+client = Client(binance_api_key, binance_api_secret)
+fixes = [{'coinmarketcap':'MIOTA', 'binance': 'IOTA'},{'coinmarketcap':'BCH', 'binance': 'BCC'}]
+
+polo = poloniex.Poloniex(poloniex_api_key,poloniex_api_secret)
+
+def main():
+	binance_ticker = pd.DataFrame(client.get_all_tickers())
+	poloniex_ticker = pd.DataFrame(polo.returnTicker())
+	prices = poloniex_ticker.loc["last"].values
+	columns = poloniex_ticker.columns
+	poloniex_reset = pd.DataFrame()
+	poloniex_reset['poloniex_price'] = prices
+	poloniex_reset['symbol'] = columns
+	poloniex_reset['symbol'] = poloniex_reset['symbol'].apply(lambda x: clean_poloniex_name(x))
+	merged_df = pd.merge(poloniex_reset,binance_ticker, on='symbol')
+	print(merged_df.head())
+
+
+	
+
+
+
+def clean_poloniex_name(name):
+	tmpNames = name.split("_")
+	tmp = tmpNames[-1] + tmpNames[0]
+	name = ''.join(tmp)
+	return name
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	# run = input("Type in yes to run and re-allocate binance coins \n")
+	# if run == "yes":
+	main()
