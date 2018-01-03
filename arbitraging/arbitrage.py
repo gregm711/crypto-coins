2ce831d7a5e28208b6005e0aaea1f6a7b8b363bc
gregm711 <gregm@wearevest.com>
2018-01-02 23:27:30 -0500

    ok

--- arbitraging/arbitrage.py
+++ arbitraging/arbitrage.py
@@ -11,6 +11,10 @@ import time
 
 import poloniex
 
+from kucoin.client import Client as KucoinClient
+import kucoin
+
+
 
 dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
@@ -19,6 +23,8 @@ binance_api_key = os.environ.get("BINANCE_API_KEY")
 binance_api_secret = os.environ.get("BINANCE_API_SECRET")
 poloniex_api_key = os.environ.get("POLONIEX_API_KEY")
 poloniex_api_secret = os.environ.get("POLONIEX_API_SECRET")
+kucoin_api_key = os.environ.get("KUCOIN_API_KEY")
+kucoin_api_secret = os.environ.get("KUCOIN_API_SECRET")
 
 poloniex_bitcoin_withdrawl_fee = 0.0005
 binance_bitcoin_withdrawl_fee = 0.001
@@ -27,13 +33,21 @@ binance_trading_fee = 0.001
 poloniex_trading_fee = 0.0015
 
 client = Client(binance_api_key, binance_api_secret)
+
+kucion_client = KucoinClient(kucoin_api_key, kucoin_api_secret)
+
+
+# get currencies
+currencies = kucion_client.get_currencies()
+# print(currencies)
+
+
 fixes = [{'coinmarketcap':'MIOTA', 'binance': 'IOTA'},{'coinmarketcap':'BCH', 'binance': 'BCC'}]
 
 polo = poloniex.Poloniex(poloniex_api_key,poloniex_api_secret)
 
 def main():
 	binance_ticker = pd.DataFrame(client.get_all_tickers())
-
 	poloniex_ticker = pd.DataFrame(polo.returnTicker())
 	prices = poloniex_ticker.loc["last"].values
 	columns = poloniex_ticker.columns
@@ -44,13 +58,15 @@ def main():
 	merged_df = pd.merge(poloniex_reset,binance_ticker, on='symbol')
 	merged_df['price']= pd.to_numeric(merged_df['price'], errors='coerce')
 	merged_df['poloniex_price']= pd.to_numeric(merged_df['poloniex_price'], errors='coerce')
+	merged_df['ratio'] = (merged_df['poloniex_price'] /merged_df['price'])
 	merged_df['percentage_difference'] = (merged_df['poloniex_price'] - merged_df['price']) / merged_df['poloniex_price']
 	merged_df['abs_diff'] = merged_df['percentage_difference'].apply(lambda x: math.fabs(x))
-	df = merged_df.sort_values(by=['abs_diff'], ascending=False)
-	print(df.head(20))
+	merged_df['abs_ratio'] = merged_df['ratio'].apply(lambda x: math.fabs(x))
+	df = merged_df.sort_values(by=['abs_ratio'], ascending=False)
+	print(df.head(10))
 
 
-	
+
 
 
 
